3cc764c125c72667162a338e9be5e1ffef175177
Lucia Makaiová <80852362+gulliver8@users.noreply.github.com>
2024-11-19 14:53:28 +0100

    Finished basic optimization algorithm -helping functions.

--- helping_functions.py
+++ helping_functions.py
@@ -1,19 +1,54 @@
-def create_feromone_matrix(places_list, start_feromones):
-    num_places = len(places_list)
-    #initialize a matrix with zeros
-    feromone_matrix = [[start_feromones] * num_places for _ in range(num_places)]
-    return feromone_matrix
+import random
 
-#TODO
-def update_feromones(feromone_matrix, cost, tabu_list):
-    #count the quality of solution ]
+
+def create_pheromone_matrix(place_nums, start_pheromones):
+    num_places =len(place_nums)
+    #initialize a matrix with zeros
+    pheromone_matrix = [[start_pheromones] * num_places for _ in range(num_places)]
+    return pheromone_matrix
+
+def update_pheromones(pheromone_matrix, cost, tabu_list):
+    #count the quality of solution
+    cost_per_move = cost/len(tabu_list)
     #add to each path which the ant pased based on tabu
+    for a, b in zip(tabu_list, tabu_list[1:] + tabu_list[:1]):
+        pheromone_matrix[a][b] += (1/cost_per_move)
     return 0
-#TODO
-def count_path_cost():
+
+def count_path_cost(tabu_list, distance_matrix):
     cost = 0
+    for a, b in zip(tabu_list, tabu_list[1:] + tabu_list[:1]):
+        distance = distance_matrix[a][b]
+        cost += distance
+        #print("Cost of solution",cost)
     return cost
-#TODO
-def pick_next_place(tabu_list, feromone_matrix):
+
+def pick_next_place(tabu_list, pheromone_matrix, place_nums):
     place = 0
-    return place
+    unvisited = []
+    current_place = tabu_list[-1]
+    #create list of unvisited places
+    for place in place_nums:
+        if place not in tabu_list:
+            unvisited.append(place)
+    pheromones = []
+    #print("unvisited places: ", unvisited)
+
+    #create pheromone association list
+    for place in unvisited:
+        pheromones.append(pheromone_matrix[current_place][place])
+    total = sum(pheromones)
+    #print(pheromones)
+    #convert pheromones into probability
+    probability = []
+    for pheromone in pheromones:
+        probability.append(pheromone/total)
+    #print(probability)
+
+    #pick next place randomly with given probability
+    next_place = random.choices(unvisited, weights=probability, k=1)[0]
+    #print("next picked place:", next_place)
+    #print("tabu list =", tabu_list)
+
+    return next_place
+
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from distance_matrix import *
 from process_input import *
 from helping_functions import *
 
-start_y, start_x, n_ants, start_feromone, max_cycles = get_user_input()
+start_y, start_x, n_ants, start_pheromone, max_cycles = get_user_input()
 
 places_list = process_file(start_y, start_x)
 if not places_list:
@@ -13,6 +13,8 @@ else:
 
 # Create the distance matrix
 distance_matrix = create_distance_matrix(places_list)
+for place in places_list:
+    print(place)
 print("Number of places: " + str(len(places_list)))
 
 #dictionary of lists for each ant
@@ -22,38 +24,48 @@ for ant in range(n_ants):
     tabu_list[list_name] = list()
 #cycle counter
 cycle = 0
-
+place_nums = range(len(places_list))
+print(place_nums)
+#reset value and list of best path
+tabu_list["ant_best"] = list()
+best_cost = 0
 #init cycle
-#reset lists, add starting place, reset best cost and best list
 while cycle < max_cycles:
-    #reset value and list of best path
-    tabu_list["ant_best"] = list()
-    best_cost = 0
     #reset list of each ant (add starting position)
     for ant in range(n_ants):
         list_name = f"ant_{ant}"
         tabu_list[list_name].clear()
         tabu_list[list_name].append(0)
 
-    #set beggining feromone intesity between places -new matrix
-    feromone_matrix = create_feromone_matrix(places_list, start_feromone)
+    #set beggining pheromone intesity between places -new matrix
+    pheromone_matrix = create_pheromone_matrix(places_list, start_pheromone)
 
     for i in range(len(places_list)-1):
         for ant in range(n_ants):
-            #TODO
-            next_place = pick_next_place(tabu_list[f"ant_{ant}"], feromone_matrix)
+            #choose next place for and to visit based on pheromone matrix
+            next_place = pick_next_place(tabu_list[f"ant_{ant}"], pheromone_matrix, place_nums)
             tabu_list[f"ant_{ant}"].append(next_place)
+
     #for each ant count the total path cost (counting also end to start path)
     for ant in range(n_ants):
-        #TODO
-        cost = count_path_cost()
-        #TODO
-        update_feromones(feromone_matrix, cost, tabu_list[f"ant_{ant}"])
-        # set new shortest path and length (compare with old one)
-        if cost < best_cost:
+        #count cost of path the ant has chosen
+        cost = count_path_cost(tabu_list[f"ant_{ant}"], distance_matrix)
+        print("Cost of ant ", ant, " in cycle", cycle, " is:", cost)
+        print("Path of ant ", tabu_list[f"ant_{ant}"])
+        #update pheromone values
+        update_pheromones(pheromone_matrix, cost, tabu_list[f"ant_{ant}"])
+        #set new shortest path and length (compare with old one)
+        print(cost, best_cost)
+        if cost < best_cost or best_cost == 0:
             best_cost = cost
+            print("path before", tabu_list["ant_best"])
             tabu_list["ant_best"] = tabu_list[f"ant_{ant}"].copy()
+            print("path before", tabu_list["ant_best"])
+    print("Best solution after ", cycle, " cycles is:", tabu_list["ant_best"])
+    print("With the cost: ", best_cost)
     cycle += 1
+print("Final solution is: ", tabu_list["ant_best"])
+print("With the cost: ", best_cost)
 
 
 
